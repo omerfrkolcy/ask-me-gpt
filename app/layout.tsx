3aa5db4a7af85f7372580a60de87a0c2c1687655
omerfrkolcy <omerfrkolcy@hotmail.com>
2023-11-19 19:19:22 +0300

    adding page analytics package

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
 import { Header } from '@/components/header'
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -46,6 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             {/* @ts-ignore */}
             <Header />
             <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
+            <Analytics />
           </div>
           <TailwindIndicator />
         </Providers>
